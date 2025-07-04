223d7eceae4619efd4d579382fef9bd25a7f732c
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-03 22:43:08 -0400

    added api call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,16 @@ function Home() {
     const [loading, setLoading] = useState(false);
     const [recipeList, setRecipeList] = useState([]);
 
-
+    async function getRecipes() {
+        try {
+            setLoading(true);
+            const res = await fetch('https://dummyjson.com/recipes');
+            const data = await res.json();
+        } catch(e) {
+            console.log(e);
+            setLoading(false);
+        }
+    }
 
     return (
         <div>
