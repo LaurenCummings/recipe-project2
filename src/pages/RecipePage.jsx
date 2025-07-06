369f889d348674a7382feea9b667a888bb0c4dc0
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-05 23:30:40 -0400

    added recipePage content

--- src/pages/RecipePage.jsx
+++ src/pages/RecipePage.jsx
@@ -8,7 +8,29 @@ function RecipePage() {
     return (
         <div className="recipe-page">
             <h2>{recipe.name}</h2>
+            <p>Difficulty: {recipe.difficulty}</p>
+            <p>Rating: {recipe.rating}</p>
+            <p>Review Count: {recipe.reviewCount}</p>
+            <p>Cook Time: {recipe.cookTimeMinutes} minutes</p>
+            <p>Servings: {recipe.servings}</p>
+            <p>{recipe.caloriesPerServing} Calories per serving</p>
             <img src={recipe.image} alt={recipe.name} />
+            <h3>Ingredients</h3>
+            {
+                recipe.ingredients.map((item, index) => {
+                    return (
+                        <p key={index}>{item}</p>
+                    )
+                })
+            }
+            <h3>Instructions</h3>
+            {
+                recipe.instructions.map((item, index) => {
+                    return (
+                        <li key={index}>{item}</li>
+                    )
+                })
+            }
         </div>
     )
 }
