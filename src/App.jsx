4c2d3f35da767bbcd6a01edce096e82e6a03372f
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-05 23:08:00 -0400

    added recipePage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import RecipePage from './pages/RecipePage';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:recipe" element={<RecipePage />} />
       </Routes>
     </div>
   )
