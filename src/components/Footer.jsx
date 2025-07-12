ff891325280475a8de9729d69846d7c099130097
Lauren Cummings <Lauren.Cummings7@gmail.com>
2025-07-12 00:10:36 -0400

    added dummyJSON link to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ function Footer() {
     return (
         <div className="footer">
             <p><a target="_blank" href="https://icons8.com/icon/esCbvDI4xJzo/cooking">Cooking</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></p>
+            <p>Recipe Data provided by <a href="https://dummyjson.com/">DummyJSON</a></p>
         </div>
     )
 }
